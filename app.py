3c527e48a83d8ac0d636cfd9317e3b44a6554eb7
Artem Surzhenko <114075751+artemsa223@users.noreply.github.com>
2025-11-14 14:27:21 +0000

    FIXED: Use environment variable for secret key

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
 import sqlite3
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, Response, redirect, url_for, flash, session, send_from_directory, abort, send_file
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
+# Load .env file
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = 'trump123'  # Set a secure secret key
+# FIXED: Use environment variable for secret key
+app.secret_key = os.environ.get('SECRET_KEY')
 
 # Configure the SQLite database
 db_path = os.path.join(os.path.dirname(__file__), 'trump.db')
@@ -187,9 +191,6 @@ def login():
     return render_template('login.html')
 
 
-
-
-
 # Logout route
 @app.route('/logout')
 def logout():
@@ -198,7 +199,6 @@ def logout():
     return redirect(url_for('index'))
     
 from flask import session
-
 
 if __name__ == '__main__':
     initialize_database()  # Initialize the database on application startup if it doesn't exist
